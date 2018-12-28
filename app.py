66ce7536a042b94562f09606223c97f860895e5e
Deepak Acharya Santhekatte <aacharyadeepak100@gmail.com>
2018-12-28 07:54:50 +0530

    added multi route single handler

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
-from bottle import Bottle, run
+from bottle import Bottle, run,template
 app=Bottle()
 @app.route('/hello')
 def hello():
     return "<h1>Hello World</h1>"
+
+@app.route('/')
+@app.route('/test/<name>')
+def test(name='Deepak Acharya'):
+    return template('<h3>Hello {{name}}, Welcome to Bottle World!</h3>',name=name)
 
 run(app, host='localhost', port=5556, debug=True)
